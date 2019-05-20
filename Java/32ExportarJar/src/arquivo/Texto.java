370d5292b3b46c8c933eb091be20a63e354d622d
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2019-05-20 17:21:37 -0300

    Exportando e importando jars

---
+++ Java/32ExportarJar/src/arquivo/Texto.java
@@ -0,0 +1,18 @@
+package arquivo;
+
+public class Texto {
+
+	//Atributo
+	private String palavra;
+	
+	//Construtor
+	public Texto(String palavra) {
+		this.palavra = palavra;
+	}
+	
+	//Método para contar letras
+	public void contarLetras() {
+		System.out.println(palavra.length());
+	}
+	
+}
---
+++ Java/33ImportarJar/src/Principal.java
@@ -0,0 +1,14 @@
+import arquivo.Texto;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		//Trabalhando com a classe texto
+		Texto t = new Texto("Proway");
+		t.contarLetras();
+		
+		
+	}
+
+}
